2b6f0218a28c2533645c8cd52b094f5f7a22a0c6
Charmaine Henderson <40720823+webcleaner@users.noreply.github.com>
2020-03-10 12:23:56 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+ debugger;
+        var text = "";
+        for (var i = 1; i <= 100 ; i++) {                 //create a for loop that will iterate through 1 to 100
+           
+             
+          if (i % 3 === 0 && i % 5 === 0) {    //if i is divisible by 3 and 5, alert CracklePop
+    
+           text+= i + " CracklePop" + "<br>" ;
+           document.getElementById("demo").innerHTML = text;
+         // console.log (i + " CracklePop");
+         
+        } else if (i%3===0) {         //If a #, when divided by 2, has a remainder of 0, number even. 
+              //div 5 === 5, 15, 25, 35, 45, 55, 65, 75, 85, 95     
+           var crackle =   i + " Crackle";  //replace with alert
+           text += i + " Crackle" + "<br>";
+           document.getElementById("demo").innerHTML = text;
+         // console.log ( i + " Crackle");
+         
+        } else if (i%5===0){    //numbers divisible by 5, contains last digits 0 or 5
+           var pop = i + " Pop";
+           text += i + " Pop" + "<br>";
+           document.getElementById("demo").innerHTML = text;
+            
+         // console.log(i + " Pop");
+         
+          }else{         
+              
+           text += i +  "<br>";
+           document.getElementById("demo").innerHTML = text;
+         // console.log(i);
+            }
+        }
